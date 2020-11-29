1a59b17f22f5667e2b326f9d28be840b34cd179b
Joaquim <jogi@itu.dk>
2020-11-29 23:27:03 +0100

    Fetch nasa weather events with axios and refresh API KEY to prevent external usage now that .env is correctly gitignored

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 import Map from "./components/Map";
 
 function App() {
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true);
+      const res = await axios.get(
+        "https://eonet.sci.gsfc.nasa.gov/api/v3/events"
+      );
+
+      setEventData(res.data.events);
+      setLoading(false);
+    };
+
+    fetchEvents();
+  }, []);
+
   return (
     <div>
       <Map />
